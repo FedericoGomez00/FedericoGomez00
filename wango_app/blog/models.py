2c50faaa6db33783c22f2397317622bb4e9b0992
Federico Gomez <gomez00federico@gmail.com>
2022-08-18 20:16:08 -0300

    IndexView_index.html_do_they_work

--- wango_app/blog/models.py
+++ wango_app/blog/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    blog_text = models.CharField(max_length=255)
+    pub_date = models.DateField("date published")
+
+    def __str__(self) -> str:
+        return self.title
+
+    def was_published_recently(self):
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- wango_app/blog/views.py
+++ wango_app/blog/views.py
@@ -1,3 +1,12 @@
+from django.utils import timezone
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blog
+
+class IndexView(generic.ListView):
+    template_name: str = "blog/index.html"
+    context_object_name: str = "latest_blog_list"
+
+    def get_queryset(self):
+        return Blog.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")
